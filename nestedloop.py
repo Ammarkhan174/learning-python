ff191141bf3c7ffc7fd6478b56cb93ffc4aac7da
Ammarkhan174 <mohd.ammar174@gmail.com>
2023-11-04 15:31:00 +0530

    nestedloop

--- nestedloop.py
+++ nestedloop.py
@@ -22,7 +22,100 @@
 # 12345
 # 12345
 
-for i in range (1,11):
-    for j in range (1,11):
-        print(j,end = "")
-    print()
+# for i in range (1,11):
+#     for j in range (1,11):
+#         print(j,end = "")
+#     print()
+
+# 01 02 03 04 05 
+# 06 07 08 09 10
+# 11 12 13 14 15
+# 16 17 18 19 20
+# 21 22 23 24 25
+# count = 1
+# for i in range (1,6):
+#     for j in range (1,6):
+#         if count<10:
+#             print("0", end = "")
+#         print(count, end = " ")
+#         count = count+1
+#     print()
+
+# 01 02 03 04 05 
+# 02 04 06 08 10 
+# 03 06 09 12 15 
+# 04 08 12 16 20 
+# 05 10 15 20 25
+
+# for i in range (1,6):
+#     for j in range (1,6):
+#         result= j*i
+#         if result<10:
+#             print("0", end = "")
+#         print(j*i, end = " ")
+#     print()
+
+# *****
+# *   *
+# *   *
+# *   *
+# *****
+# n = 5
+# for i in range (n):
+#     for j in range(n):
+#        if i==0 or i==n-1 or j==0 or j==n-1:
+#            print("*", end="")
+#        else:
+#            print(" ", end="")
+#     print()
+
+# n = 5
+# for i in range (n):
+#     for j in range(n):
+#        if i==0 or i==n-1 or j==0 or j==n-1:
+#            print("*", end="")
+#        else:
+#            print(" ", end="")
+#     print()
+# n = 5
+# for i in range (n):
+#     for j in range(n):
+#        if i==0 or i==n-1 or j==0 or j==n-1:
+#            print("*", end="")
+#        else:
+#            print(" ", end="")
+#     print()
+# ********
+# **    **
+# * *  * *
+# *  **  *
+# *  **  *
+# * *  * *
+# ** ** **
+# n = 5
+# for i in range (n):
+#      for j in range(n):
+#         if i==0 or i==n-1 or j==0 or j==n-1 or i==j or i+j==n-1:
+#             print("*", end="")
+#         else:
+#             print(" ", end="")
+#      print()
+
+# n = 5
+# for i in range (n):
+#     for j in range (n):
+#         if i==0:
+#             print("*", end = "")
+#         else:
+#             print(" ", end ="")
+#         print()
+
+for i in range (1,9):
+    for j in range (1,9):
+        if i*j:
+            print("", end="")
+        else:
+            print(" ", end="")
+        print()
+
+
--- test.py
+++ test.py
@@ -31,7 +31,17 @@ a = tem
 print(a)
 print(b)
 
+a = 10
+b = 20
+c = 30
 
-
+if a>=b and a>=c:
+    print("a is greater than b or c")
+elif b>=a and b>=c:
+    print("b is greater than a or c")
+elif c>=a and c>=b:
+    print("c is greater than b or a")
+else :
+    print("")
 
 
